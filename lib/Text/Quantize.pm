59eca13b868c1a176e8650bb201577330d6d44a0
Shawn M Moore <code@sartak.org>
2012-11-14 13:34:17 -0500

    endpoints_for

--- lib/Text/Quantize.pm
+++ lib/Text/Quantize.pm
@@ -26,4 +26,41 @@ sub bucketize {
     return \%buckets;
 }
 
+sub _endpoints_for {
+    my $buckets = shift;
+    my ($min_endpoint, $max_endpoint);
+
+    my @sorted_buckets = (sort { $a <=> $b } keys %$buckets);
+    my $min = $sorted_buckets[0];
+    my $max = $sorted_buckets[-1];
+
+    if ($min == 0) {
+        $min_endpoint = -1;
+    }
+    elsif ($min == 1) {
+        $min_endpoint = 0;
+    }
+    elsif ($min < 0) {
+        $min_endpoint = -1 * (2 ** (int(log(-$min) / log(2)) + 1));
+    }
+    else {
+        $min_endpoint = 2 ** (int(log($min) / log(2)) - 1);
+    }
+
+    if ($max == 0) {
+        $max_endpoint = 1;
+    }
+    elsif ($max == -1) {
+        $max_endpoint = 0;
+    }
+    elsif ($max < 0) {
+        $max_endpoint = -1 * (2 ** (int(log(-$max) / log(2)) - 1));
+    }
+    else {
+        $max_endpoint = 2 ** (int(log($max) / log(2)) + 1);
+    }
+
+    return ($min_endpoint, $max_endpoint);
+}
+
 1;
