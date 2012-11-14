102e57b34ee5363d31220396c5eabbfdd3bafd45
Shawn M Moore <code@sartak.org>
2012-11-14 12:04:10 -0500

    Test next_power_of_2

--- lib/Text/Quantize.pm
+++ lib/Text/Quantize.pm
@@ -3,4 +3,25 @@ package Text::Quantize;
 use strict;
 use warnings;
 
+sub _next_power_of_2 {
+    my $num = int(shift);
+    my $sign = 1;
+
+    if ($num == 0) {
+        return 0;
+    }
+
+    if ($num < 0) {
+        $sign = -1;
+        $num *= -1;
+    }
+
+    my $pow = 1;
+    while ($num >= $pow) {
+        $pow *= 2;
+    }
+
+    return $sign * $pow;
+}
+
 1;
